6a14c86c17f0eadb2e87717b230a725eb4728b33
parthoMalakar <parthmalakar4@gmail.com>
2021-01-22 12:34:11 +0530

    Added Props to cart Item

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cart from './Cart'
 function App() {
   return (
     <div className="App">
-      <h1>Cart</h1>
+      {/* <h1>Cart</h1> */}
       <Cart/>
     </div>
   );
--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,55 @@ import React from 'react';
 import CartItem from './CartItem'
 
 class Cart extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 999,
+                    title: 'Watch',
+                    qty: 1,
+                    img: "",
+                    id: 1
+                },
+                {
+                    price: 9999,
+                    title: 'Mobile Phone',
+                    qty: 2,
+                    img: "",
+                    id: 2
+                },
+                {
+                    price: 99999,
+                    title: 'Laptop',
+                    qty: 0,
+                    img: "",
+                    id: 3
+                }
+            ]
+        }
+
+         // this.increaseQuantity = this.increaseQuantity.bind(this);
+         // this.testing();
+    }
+
     render () {
+        const {products} = this.state;
         return (
+             
             <div className='cart'>
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                {products.map((product) => {
+                    return (
+                    <CartItem 
+                        product={product} 
+                        key={product.id}
+                        // func={ () => console.log("Testing a function in CartItem Component")}
+                        // isLoggedIn ={false}
+                        // jsx={<h1>Test</h1>}
+                        
+                    />
+                    )
+                })}
             </div>
         ) 
     }
